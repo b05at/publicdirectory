a10a0f9f5a28d1190c43594788ee2e6f5ff499f0
b05at <60232154+b05at@users.noreply.github.com>
2020-10-28 16:50:55 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-cron -n
+#!/bin/sh
+
+#fix link-count, as cron is being a pain, and docker is making hardlink count >0 (very high)
+touch /etc/crontab /etc/cron.*/*
+
+service cron start
+
+# Hand off to the CMD
+exec "$@"
